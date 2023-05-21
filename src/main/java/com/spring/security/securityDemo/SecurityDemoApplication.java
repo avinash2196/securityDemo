83431b9f2f1182c57cfe21a8734a34a156d8422b
avinash2196 <avinash21961@gmail.com>
2023-05-21 15:00:52 -0400

    commit with exclusion

--- src/main/java/com/spring/security/securityDemo/SecurityDemoApplication.java
+++ src/main/java/com/spring/security/securityDemo/SecurityDemoApplication.java
@@ -2,8 +2,9 @@ package com.spring.security.securityDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SecurityDemoApplication {
 
 	public static void main(String[] args) {
