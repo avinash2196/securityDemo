cc394ed5193b3f2d18d3a93f44feb765ccb698d1
avinash2196 <avinash21961@gmail.com>
2023-05-22 00:45:49 -0400

    updating folder structure

--- src/main/java/com/spring/security/securityDemo/config/CustomWebSecurityConfig.java
+++ src/main/java/com/spring/security/securityDemo/config/CustomWebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.spring.security.securityDemo;
+package com.spring.security.securityDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
