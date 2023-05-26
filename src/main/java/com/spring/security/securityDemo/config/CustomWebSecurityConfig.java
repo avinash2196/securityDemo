d4f5c61e367fb16eedbc83ec7b62b6179d58e7a2
avinash2196 <avinash21961@gmail.com>
2023-05-26 03:32:13 -0400

    removed additional codes

--- src/main/java/com/spring/security/securityDemo/config/CustomWebSecurityConfig.java
+++ src/main/java/com/spring/security/securityDemo/config/CustomWebSecurityConfig.java
@@ -1,17 +1,13 @@
 package com.spring.security.securityDemo.config;
 
-import com.spring.security.securityDemo.service.UserDetailService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,8 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @AllArgsConstructor
 public class CustomWebSecurityConfig {
 
-    private final UserDetailService userDetailService;
-    private final PasswordEncoder passwordEncoder;
     private final JWTAuthFilter jwtAuthFilter;
     private final AuthEntryPointImpl authEntryPoint;
     private static final String[] AUTH_WHITELIST = {
@@ -56,14 +50,6 @@ public class CustomWebSecurityConfig {
                 .build();
     }
 
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        return authenticationProvider;
-    }
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
